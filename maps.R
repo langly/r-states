8fcc3741b07eb384c5245f869bbafba90da1f997
Kenneth Ostby <langly@langly.org>
2024-06-15 08:12:24 -0700

    Added some more states:
    - Also cleaned up the look a bit.

--- maps.R
+++ maps.R
@@ -3,13 +3,16 @@ library(maps)
 library(mapdata)
 library(mapproj)
 
-read.table(text="region St-Abbr colors
-                 oregon       OR    #00931e
-                 washington   WA    #00931e
-                 california   CA    #00931e
-                 arizona      AR    #00931e
-                 nevada       NV    #00931e
-                 texas        TX    #00931e",
+read.table(text='region St-Abbr colors
+                 wisconsin          WI    #00931e
+                 "new york"         NY    #00931e
+                 massachusetts      MA    #00931e
+                 oregon             OR    #00931e
+                 washington         WA    #00931e
+                 california         CA    #00931e
+                 arizona            AR    #00931e
+                 nevada             NV    #00931e
+                 texas              TX    #00931e',
            stringsAsFactors=FALSE, header=TRUE, comment.char="") -> df
 
 state <- map_data('state')
@@ -24,5 +27,7 @@ ggplot() +
     geom_map(data=df, map=state,
                 aes(fill="blue", map_id=region),
                 color="#2b2b2b", linewidth=0.15) +
-    #scale_color_identity() +
-    coord_map("polyconic")
+    scale_color_identity() +
+    coord_map("polyconic") +
+    theme_void() +
+    theme(legend.position="none")
